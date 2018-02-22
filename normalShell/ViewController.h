b24d9f6b3625c8d8621292220c3fb5768b7e5ea2
Seven <Seven@seven-3.local>
2018-02-22 14:06:19 +0800

    Initial Commit

---
+++ normalShell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  normalShell
+//
+//  Created by Seven on 2018/2/22.
+//  Copyright © 2018年 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ normalShell/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  normalShell
+//
+//  Created by Seven on 2018/2/22.
+//  Copyright © 2018年 Seven. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
